0853a6da1205c00f0f4f9b083b360489eb924099
mrustam <mrustam9929@gmail.com>
2025-09-03 15:46:24 +0500

    - added companies app and models - added naff app and models

---
+++ src/apps/companies/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from apps.companies.enums import BotModules
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        db_table = 'companies'
+
+
+class CompanyBot(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    bot_module = models.CharField(choices=BotModules.choices, max_length=255)
+    name = models.CharField(max_length=255)
+    _token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'companies_bot'
+
+    @property
+    def token(self) -> str:
+        return self._token
+
+    @token.setter
+    def token(self, value):
+        self._token = value
